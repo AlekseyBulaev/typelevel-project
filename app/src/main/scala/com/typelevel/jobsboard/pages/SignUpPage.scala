66235a4c8d6c9bf6012141c921b922281a89a2de
AlekseyBulaev <alexeybulaev@gmail.com>
2024-05-20 10:58:55 -0400

    Done - 41 Signing Up

--- app/src/main/scala/com/typelevel/jobsboard/pages/SignUpPage.scala
+++ app/src/main/scala/com/typelevel/jobsboard/pages/SignUpPage.scala
@@ -3,11 +3,115 @@ package com.typelevel.jobsboard.pages
 import cats.effect.*
 import tyrian.*
 import tyrian.Html.*
+import tyrian.cmds.Logger
 
-final case class SignUpPage() extends Page {
+import com.typelevel.jobsboard.common.*
+// form
+/*
+    input
+      - email
+      - password
+      - confirm password
+      - first name
+      - last name
+      - company
+    button to trigger a sign up
+ */
+final case class SignUpPage(
+    email: String = "",
+    password: String = "",
+    confirmPassword: String = "",
+    firstName: String = "",
+    lastName: String = "",
+    company: String = "",
+    status: Option[Page.Status] = None,
+) extends Page {
+  import SignUpPage.*
+
   override def initCmd: Cmd[IO, Page.Msg] = Cmd.None // TODO
 
-  override def update(msg: Page.Msg): (Page, Cmd[IO, Page.Msg]) = (this, Cmd.None) // TODO
+  override def update(msg: Page.Msg): (Page, Cmd[IO, Page.Msg]) = msg match {
+    case UpdateEmail(e)            => (this.copy(email = e), Cmd.None)
+    case UpdatePassword(p)         => (this.copy(password = p), Cmd.None)
+    case UpdateConfirmPassword(cp) => (this.copy(confirmPassword = cp), Cmd.None)
+    case UpdateFirstName(fn)       => (this.copy(firstName = fn), Cmd.None)
+    case UpdateLastName(ln)        => (this.copy(lastName = ln), Cmd.None)
+    case UpdateCompany(c)          => (this.copy(company = c), Cmd.None)
+    case AttemptSignUp =>
+      if(!email.matches(Constants.emailRegex)) (setErrorStatus("Email is invalid"), Cmd.None)
+      else if (password.isEmpty) (setErrorStatus("Please enter a password"), Cmd.None)
+      else if (password != confirmPassword) (setErrorStatus("Password fields do not match"), Cmd.None)
+      else (this, Logger.consoleLog[IO]("SIGNING UP!", email, password, firstName, lastName, company))
+    case _                         => (this, Cmd.None)
+  }
 
-  override def view(): Html[Page.Msg] = div("Sign Up page - TODO")
+  override def view(): Html[Page.Msg] =
+    div(`class` := "form-section")(
+      // title: Sign Up
+      div(`class` := "top-section")(
+        h1("Sign Up")
+      ),
+      // form
+      form(
+        name    := "signup",
+        `class` := "form",
+        onEvent(
+          "submit",
+          e => {
+            e.preventDefault()
+            NoOp
+          }
+        )
+      )(
+        // 6 inputs
+        renderInput("Email", "email", "text", true, UpdateEmail(_)),
+        renderInput("Password", "password", "password", true, UpdatePassword(_)),
+        renderInput("Confirm Password", "cpassword", "password", true, UpdateConfirmPassword(_)),
+        renderInput("First Name", "firstname", "text", true, UpdateFirstName(_)),
+        renderInput("Last Name", "lastname", "text", true, UpdateLastName(_)),
+        renderInput("Company", "company", "text", true, UpdateCompany(_)),
+        // button
+        button(`type` := "button", onClick(AttemptSignUp))(" Sign Up")
+      ),
+      status.map(s => div(s.message)).getOrElse(div())
+    )
+
+  ///////////////////////////////////////////////////////////////////////////////////
+  ///////  private
+  ///////////////////////////////////////////////////////////////////////////////////
+
+  // UI
+  private def renderInput(
+      name: String,
+      uid: String,
+      kind: String,
+      isRequired: Boolean,
+      onChange: String => Msg
+  ) =
+    div(`class` := "form-input")(
+      label(`for` := name, `class` := "form-label")(
+        if (isRequired) span("*") else span(),
+        text(name)
+      ),
+      input(`type` := kind, `class` := "form-control", id := uid, onInput(onChange))
+    )
+
+  // util
+  def setErrorStatus(message: String): Page = this.copy(status = Some(Page.Status(message = message, kind = Page.StatusKind.ERROR)))
+}
+
+object SignUpPage {
+  trait Msg                                                 extends Page.Msg
+  case class UpdateEmail(email: String)                     extends Msg
+  case class UpdatePassword(password: String)               extends Msg
+  case class UpdateConfirmPassword(confirmPassword: String) extends Msg
+  case class UpdateFirstName(firstName: String)             extends Msg
+  case class UpdateLastName(lastName: String)               extends Msg
+  case class UpdateCompany(company: String)                 extends Msg
+
+  // actions
+  case object AttemptSignUp extends Msg
+  case object NoOp                                          extends Msg
+
+
 }
